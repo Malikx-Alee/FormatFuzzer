337a1c2a8e2ae3c3e86267142d6154fb5ef864a8
Malik Ali Haider Awan <ali114_haider@outlook.com>
2026-02-25 06:25:48 +0100

    detect checksum uncommented

--- learning_constraints/checksum_detector.py
+++ learning_constraints/checksum_detector.py
@@ -60,12 +60,12 @@ class ChecksumDetector:
             self._detect_bmp_compression(file_data, start, end, attribute_keys, last_key)
         
         # Detect checksum algorithms if enabled
-        # if getattr(Config, "ENABLE_CHECKSUM_DETECTION", False):
-        #     self._detect_checksum(
-        #         file_data, byte_ranges, original_byte_ranges,
-        #         start, attribute_keys, byte_values,
-        #         seen_chunk_types_for_file
-        #     )
+        if getattr(Config, "ENABLE_CHECKSUM_DETECTION", False):
+            self._detect_checksum(
+                file_data, byte_ranges, original_byte_ranges,
+                start, attribute_keys, byte_values,
+                seen_chunk_types_for_file
+            )
     
     def _detect_png_compression(self, file_data: bytes, end: int, attribute_keys: list, last_key: str):
         """Detect PNG compression method from IHDR chunk."""
